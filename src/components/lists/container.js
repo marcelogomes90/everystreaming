e94d04c9cde0891482572cfabc855cdf647f8932
Marcelo <marcelo.sobrinho@outlook.com>
2022-10-02 00:16:01 -0300

    chore: fix lists padding

--- src/components/lists/container.js
+++ src/components/lists/container.js
@@ -2,6 +2,10 @@ import styled from "styled-components";
 
 const Container = styled.div`
     padding: 0 3rem 0 3rem;
+
+    @media (max-width: 767px) {
+        padding: 0 1rem 0 1rem;
+    }
 `
 
 export default Container;
