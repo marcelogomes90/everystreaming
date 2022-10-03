d1d6392cf3e414734883d826a263b52dca20219f
Marcelo Gomes <94990663+marcelogomes90@users.noreply.github.com>
2022-10-03 07:07:50 -0300

    chore: fix text size

--- src/components/footer/text.js
+++ src/components/footer/text.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { FontColor1 } from "../../style/colors";
 
 const Text = styled.p`
-    font-size: 1rem;
+    font-size: 0.75rem;
     color: ${FontColor1};
     margin: 0;
 `
