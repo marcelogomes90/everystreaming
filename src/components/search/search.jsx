b8f1fbabd5c8fb4863e1b717373bd7ca42c8e824
Marcelo <marcelo.sobrinho@outlook.com>
2022-10-05 18:23:09 -0300

    chore: set modal in search

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -6,17 +6,27 @@ import Poster from "./poster";
 import Title from "./title";
 import Container from "./container";
 import DivResults from "./divResults";
+import Modal from "../modal/modal";
 
 function Search() {
 
     const [searchText, setSearchText] = useState("");
     const [searchData, setSearchData] = useState();
+    const [modalOpen, setModalOpen] = useState(false);
+    const [classe, setClasse] = useState();
+    const [id, setId] = useState();
 
     const searchURL = `https://api.themoviedb.org/3/search/multi?api_key=253799727221b7a1aa90c66eb08832a0&language=pt-BR&include_adult=false&query=${searchText}`
     const imageURL = "https://image.tmdb.org/t/p/w154"
 
     const makeSearch = (event) => {
         setSearchText(event.target.value);
+    }
+
+    const showModal = (e) => {
+        setClasse(e.target.className);
+        setId(e.target.id);
+        setModalOpen(true);
     }
 
     useEffect(() => {
@@ -36,7 +46,10 @@ function Search() {
             { searchText != "" ?
             <DivResults>
                 { searchData?.filter(dataFilter => dataFilter.media_type != "person").slice(0, 10).map(result => (
-                    <DivSearch>
+                    <DivSearch 
+                        id={result.id}
+                        className={`${result.media_type}-jwds`} 
+                        onClick={(e) => showModal(e)}>
                         <Poster src={`${imageURL}${result.poster_path}`}></Poster>
                         <Title>{result.name || result.title}</Title>
                     </DivSearch>
@@ -44,6 +57,7 @@ function Search() {
             </DivResults>
             :
             null }
+            { modalOpen ? <Modal setModalOpen={setModalOpen} id={id} classe={classe}></Modal> : null }
         </Container>
     )
 }
